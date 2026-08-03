9086cc1616f56b724406f7f01e521e263ba86695
Rafael Alves <alves540@hotmail.com>
2026-08-03 16:21:53 -0300

    Mudanças

---
+++ argumentosnomeados.py
@@ -0,0 +1,15 @@
+"""
+Argumentos nomeados e não nomeados em funções em Python
+Argummento nomeado tem nome com sinal de igual
+Argumento não nomeado recebe apenas o argumento (valor)
+Refatorar: editar o seu código.
+"""
+
+
+def soma(x , y, z):
+    #Definição
+    print(f'{x=}  y={y} {z=}','|','x + y + s =', x + y + z )
+
+soma(1, 2, 3)
+soma(2, 1, z=5)
+soma(100, 200, 300)
